1a89a6d040a30c30b9db4a2a98c25ab3356deb57
runtimeanxiety <winkie.storm@gmail.com>
2025-09-21 20:43:45 +0800

    added textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
+    print(node)
+
+
+main()
